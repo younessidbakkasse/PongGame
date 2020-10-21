ef310e263066987fb9797fac28fdd6e8748894dc
younessidbakkasse <std.youness@gmail.com>
2020-10-21 18:24:00 +0200

    added speed variation

--- main.py
+++ main.py
@@ -13,27 +13,37 @@ screenWidthY = 540
 orange = pygame.Color(255, 127, 80)
 
 # Player coords
-playerX = 50
-playerY = 10
+playerX = screenWidthX/2
+playerY = screenWidthY/2
 radius = 10
+playerSpeedX = 0
+playerSpeedY = -1
 
 Clock = pygame.time.Clock()
 
 def player(x, y, r):
-    pygame.draw.circle(screen, orange, (x, y), r)
+    pygame.draw.circle(screen, orange, (int(x), int(y)), r)
 
 # the main game loop
 while True:
     # maintain the game screen open
     screen = pygame.display.set_mode((screenWidthX, screenWidthY))
     
-    # closing the pygame display event 
+    # pygame display events 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerSpeedX = -1
+            elif event.key == pygame.K_RIGHT:
+                playerSpeedX = 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                playerSpeedX = 0
 
-    playerX += 1
+    playerX += playerSpeedX
     Clock.tick(60)
     player(playerX, playerY, radius)
     pygame.display.update()
